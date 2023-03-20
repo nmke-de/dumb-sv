15191c18f292834ddacbcb9d55f6f7651074451a
nmke-de <github@nmke.de>
2023-03-20 08:41:15 +0100

    Added option to stop supervising
    By killing the supervisor with SIGINT,
    the supervisor kills its child and then
    returns.

--- sv.c
+++ sv.c
@@ -1,11 +1,24 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
 
 extern char **environ;
 #define sys(args) execve(*(args), (args), environ)
 
 #define print(str) write(1, (str), strlen((str)))
+
+// Kill child process and stop supervising.
+pid_t child;
+char *childname;
+int running = 1;
+void fine(int signum) {
+	print("Killing ");
+	print(childname);
+	print("\n");
+	kill(child, SIGINT);
+	running = 0;
+}
 
 // Supervise a single service without arguments
 int main(int argc, char **argv) {
@@ -18,12 +31,17 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < argc - 1;)
 		argv[i] = argv[++i];
 	argv[argc - 1] = NULL;
+	// Prepare signal handler
+	childname = *argv;
+	signal(SIGINT, fine);
 	// Main loop. Wait until child quits before restarting.
 	for (;;) {
-		pid_t child = fork();
+		child = fork();
 		if (child == 0)
 			sys(argv);
 		wait(&child);
+		if (!running)
+			return 0;
 		print("Restarting ");
 		print(argv[0]);
 		print("...\n");
